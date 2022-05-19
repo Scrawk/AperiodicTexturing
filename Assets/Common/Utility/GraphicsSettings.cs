0305e329bb974c7b92f8bc50f61345bdc3b49b4b
Justin <29545213+Scrawk@users.noreply.github.com>
2022-05-19 11:50:20 +0800

    update

---
+++ Assets/Common/Utility/GraphicsSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Common.Unity.Utility
+{
+    [ExecuteInEditMode]
+    [AddComponentMenu("Common/Utility/Graphics Settings")]
+    public class GraphicsSettings : MonoBehaviour
+    {
+
+        public int vSyncCount = 1;
+
+        public int targetFrameRate = 60;
+
+        private void Update()
+        {
+            QualitySettings.vSyncCount = vSyncCount;
+            Application.targetFrameRate = targetFrameRate;
+        }
+
+    }
+
+}
